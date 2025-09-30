4498e81371026f43bc0aaada973b386351f09dee
cremacious <chrismackall3@gmail.com>
2025-09-30 08:50:42 -0400

    Successful user creation and login/logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,11 +6,13 @@ export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
     if (!fullName || !email || !password) {
+      console.log('Missing fields:', { fullName, email, password });
       return res
         .status(400)
         .json({ success: false, message: 'All fields are required' });
     }
     if (password.length < 6) {
+      console.log('Password is too short:', { password });
       return res.status(400).json({
         success: false,
         message: 'Password must be at least 6 characters',
@@ -26,6 +28,7 @@ export const signup = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({ fullName, email, password: hashedPassword });
+    console.log('Creating new user:', { fullName, email });
     if (newUser) {
       generateToken(newUser._id, res);
       await newUser.save();
@@ -37,14 +40,39 @@ export const signup = async (req, res) => {
       });
     }
   } catch (error) {
+    console.error('Error creating user:', error);
     res
       .status(500)
       .json({ success: false, message: 'Error creating user', error });
   }
 };
-export const login = (req, res) => {
-  res.send('Login controller');
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
 };
-export const logout = (req, res) => {
-  res.send('Logout controller');
+export const logout = async (req, res) => {
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
 };
