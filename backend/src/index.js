5a2e610c6be35af0a9bec000cd19a8551cff7687
cremacious <chrismackall3@gmail.com>
2025-10-10 12:41:40 -0400

    Setup for Socket IO

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ import dotenv from 'dotenv';
 import { connectDB } from '../lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { app, server } from '../lib/socket.js';
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json({ limit: '10mb' }));
@@ -22,7 +23,7 @@ app.use(
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
