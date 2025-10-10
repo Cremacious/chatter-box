95d683edd2a2577c78508403c5e72757f802b7f1
cremacious <chrismackall3@gmail.com>
2025-10-10 15:59:25 -0400

    Restructured src folder

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+
 import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
 
@@ -42,7 +43,7 @@ export const sendMessage = async (req, res) => {
 
     let imageUrl;
     if (image) {
-    
+      // Upload base64 image to cloudinary
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
     }
